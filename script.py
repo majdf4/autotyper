add952d262c4249f99bd978a311fb959347c3fdf
majd <77127406+majdf@users.noreply.github.com>
2021-04-07 10:25:51 -0400

    Script upload
    uploaded code and script

---
+++ script.py
@@ -0,0 +1,52 @@
+import pyautogui
+import time
+import random
+import sys
+
+
+
+
+
+
+
+#   ███  █   █ █████  ███  █████ █   █ ████  █████ ████
+#  █   █ █   █   █   █   █   █    █ █  █   █ █     █   █
+#  █████ █   █   █   █   █   █     █   ████  ████  ████
+#  █   █ █   █   █   █   █   █     █   █     █     █   █
+#  █   █ █████   █    ███    █     █   █     █████ █   █
+#                       by majd
+# _________________________________________________________
+
+
+
+
+
+
+
+
+count=0
+pyautogui.FAILSAFE = True
+text=input("what do you want the text to be?")
+
+while True:
+     try:
+         tiime = int(input("how long should it take? (in seconds)"))
+         break
+     except ValueError:
+         print("that wasn't a valid number.  try again.")
+
+time.sleep(5)
+
+try:
+
+	while True:
+		pyautogui.write(text)
+		pyautogui.press('enter')
+		count+=1
+		time.sleep(tiime)
+
+except :
+
+	print("finished and printed {} line(s)".format(count))
+	#print("Unexpected error:", sys.exc_info()[0])
+	exit()
